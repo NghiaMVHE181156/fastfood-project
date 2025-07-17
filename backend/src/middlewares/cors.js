1e6cbbd14d380bc92ec40767509c202ffb0e8913
NghiaMVHE181156 <nghiamvhe181156@fpt.edu.vn>
2025-07-17 14:10:36 +0700

    add PATCH CORS

--- backend/src/middlewares/cors.js
+++ backend/src/middlewares/cors.js
@@ -1,7 +1,10 @@
 // Middleware CORS để cho phép frontend kết nối
 const cors = (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
